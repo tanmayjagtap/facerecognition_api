5ebff67ccef64501421881a50a50878a5d08fd4b
tanmayjagtap <tanmayjagtap27@gmail.com>
2020-11-16 23:40:29 +0530

    1.3

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.put('/image',(req,res)=>image.imageHandler(req,res,db))
 
 app.post('/imageurl',(req,res)=>image.handleAPICall(req,res))
 
-app.listen(3000, () => {
-    console.log('app running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app running on port 3000 ${process.env.PORT}`)
 });
